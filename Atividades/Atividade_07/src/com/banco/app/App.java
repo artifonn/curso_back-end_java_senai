d09be6eb312492806f3d704a371fce21d69f5dfc
Sergio Artifon <artifonni@gmail.com>
2026-04-08 21:42:42 -0300

    Aula 08/04/2026

---
+++ Atividades/Atividade_07/src/com/banco/app/App.java
@@ -0,0 +1,24 @@
+package com.banco.app;
+public class App {
+
+    public static void main(String[] args) throws Exception {
+
+        // TODO: atividade 07
+        // Crie um programa de aplicativo de banco: o usuario poderá escolher se 
+        // deseja enttrar como pessoa física ou pessoa jurídica, deverá informar o nome,
+        // email CPF (classe PessoaFisica), ou nome fantasia, razao social, email e cnjp (classe PessoaJuridica),
+        //  e o programa deverá informar a agência e conta (classe Conta) do titular (uma única vez durante todo o
+        // programa). Em seguida, o programa deverá exibir as opções para o usuário: consultar dados da conta, fazer
+        // saque, fazer depósito, sair do programa. O programa deverá cobrar umna taxa de 0,01% em cima de cada saque feito 
+        // pela pessoa juridica . Qualquer saque só poderá ser feito caso o valor do saque seja menor ou igual ao saldo da conta
+        // NOTE: Use os 4 pilares da orientacao a objeto, mais interface e composicao. E o mais importante: DIVIRTAM-SE!!!!
+
+        
+
+
+
+
+
+        
+    }
+}
---
+++ Atividades/Atividade_07/src/com/banco/models/ContaPessoaFisica.java
@@ -0,0 +1,5 @@
+package com.banco.models;
+
+public class ContaPessoaFisica {
+
+}
