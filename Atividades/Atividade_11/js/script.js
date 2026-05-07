f8f44c3075ab897f5d3579111cd5fde9839b8246
Sergio Artifon <artifonni@gmail.com>
2026-05-07 13:47:50 -0300

    atividade 11

--- Atividades/Atividade_11/js/script.js
+++ Atividades/Atividade_11/js/script.js
@@ -1,3 +1,23 @@
 // TODO: Atividade 11
 // Desenvolva uma página HTMl onde o usuário pode Calcular o seu IMC
 // NOTE: Use BootStrap para fazer uma página bem bonita.
+
+
+const form = document.querySelector("form");
+
+
+
+const calcularImc = () => {
+
+
+    let nome = document.querySelector("#nome").value;
+    let idade = document.querySelector("#idade").value;
+    let altura = document.querySelector("#altura").value;
+
+    let result = (idade/altura);
+
+    if (30 ==)
+    
+    
+
+}
