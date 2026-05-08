df07e47fc099d93bf63107d0d3281e1e69fbef1a
Sérgio Artifon <artifonn@gmail.com>
2026-05-07 21:16:57 -0300

    feat: finaliza atividade calcular imc

--- Atividades/Atividade_11/src/script.js
+++ Atividades/Atividade_11/src/script.js
@@ -1,25 +1,33 @@
 const form = document.querySelector("form");
-const botao = document.querySelector("#botao");
+const diag = document.querySelector("#diag");
 
-const calc = () => {
-  let nome = document.querySelector("#nome");
-  let peso = document.querySelector("#peso");
-  let altura = document.querySelector("#altura");
+const calcularIMC = () => {
+  let peso = document.querySelector("#peso").value.replace(',', '.');
+  let altura = document.querySelector("#altura").value.replace(',', '.');
 
-  
-		let calculo = peso / (altura * altura);
-    document.querySelector("#result").innerHTML = nome + " o seu imc é " + calculo;
+  let imc = peso / Math.pow(altura, 2);
 
-    document.querySelector("#result").innerHTML = 'Não foi possível calcular o seu imc';
-  
-
-  form.reset();
+  return imc.toFixed(2);
 };
 
-// 
+const limparInput = () => {
+  form.reset();
+}
 
-form.addEventListener('submit', function(event) {
-	event.preventDefault();
-	
-	calc();
-})
+const exiberDiag = (imc) => {
+  return (imc < 18.5) ? 'Você está abaixo do peso.' :
+         (imc < 25) ? 'Você está no peso ideal.' :
+         (imc < 30) ? 'Você está acima do peso.' :
+         (imc < 35) ? 'Você está obeso.' :
+         (imc < 40) ? 'Você está com obesidade nível II.' :
+         'Você está com obesidade mórbida.';
+};
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  
+  let imc = calcularIMC();
+
+  diag.innerHTML = `Seu IMC é: ${imc} <br> ${exiberDiag(imc)}`;
+  limparInput();
+});
